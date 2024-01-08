31fc9bb641d3cd8f4fe09c779fbb43099097cfee
KutmanbekovDooron <kutmanbekovdooron69@gmail.com>
2024-01-09 00:52:06 +0600

    card dsign finished

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -8,4 +8,18 @@ import { Result } from 'src/app/models/charactor';
 })
 export class CharacterComponent {
   @Input() product: Result;
+
+  getStatus() {
+    let color;
+
+    if (this.product.status == 'Alive') {
+      color = 'status_green';
+    } else if (this.product.status == 'Dead') {
+      color = 'status_red';
+    } else {
+      color = 'status_gray';
+    }
+
+    return color;
+  }
 }
